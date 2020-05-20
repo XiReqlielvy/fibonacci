c45b71e5829aefc63d9409a78820f1d3ad3cf6c1
XiReqlielvy <xireimy0116@gmail.com>
2020-05-20 13:26:53 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 'use strict';
+const len = process.argv[2] || 40;
+function fib(n){
+    if (n === 0){
+        return 0;
+    }
+    else if (n === 1){
+        return 1;
+    }
+    return fib(n -1) + fib(n - 2);
+}
+
+for (let i = 0; i <= len; i++){
+    console.log(fib(i));
+}
